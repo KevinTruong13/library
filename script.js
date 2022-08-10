9e9cd954540cd964a1fe8a32fa1ae2abc2c07061
Kevin Truong <ktruon11@montgomerycollege.edu>
2022-08-10 12:52:37 -0400

    Add hover event listeners to book cards

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const bookCards = document.querySelectorAll('article');
+bookCards.forEach(bookCard => {
+    bookCard.addEventListener('mouseenter', showHover);
+    bookCard.addEventListener('mouseleave', hideHover);
+})
+
+function showHover(e) {
+    console.log(e);
+}
+
+function hideHover(e) {
+    console.log('hi');
+}
