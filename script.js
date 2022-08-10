67893fca9ff70a2e54e8389acdedc98c9bd08577
Kevin Truong <ktruon11@montgomerycollege.edu>
2022-08-10 13:17:33 -0400

    Add Book constructor function

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 'use strict';
 
-const bookCards = document.querySelectorAll('article');
+const bookCards = document.querySelectorAll('article.hasCover');
 bookCards.forEach(bookCard => {
     bookCard.addEventListener('mouseenter', showHover);
     bookCard.addEventListener('mouseleave', hideHover);
 })
+
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = Number(pages);
+    this.isRead = Boolean(isRead);
+}
 
 function showHover(e) {
     console.log(e);
