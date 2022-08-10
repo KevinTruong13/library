eea4bf3f1c7a28e908bb4a7336db2fbfd64795ee
Kevin Truong <ktruon11@montgomerycollege.edu>
2022-08-10 13:33:54 -0400

    Remove script hover functions

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
 'use strict';
 
-const bookCards = document.querySelectorAll('article.hasCover');
-bookCards.forEach(bookCard => {
-    bookCard.addEventListener('mouseenter', showHover);
-    bookCard.addEventListener('mouseleave', hideHover);
-})
-
 function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = Number(pages);
     this.isRead = Boolean(isRead);
-}
-
-function showHover(e) {
-    console.log(e);
-}
-
-function hideHover(e) {
-    console.log('hi');
 }
