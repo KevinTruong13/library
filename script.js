09fbd73e06a5954d7de72dbf87ea40c0d07c222a
Kevin Truong <ktruon11@montgomerycollege.edu>
2022-08-12 10:26:44 -0400

    Add sample books through DOM manipulation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const books = createSampleBooks();
-displayBook(books[0], 0);
+books.forEach(displayBook);
 
 function Book(title, author, pages, isRead, coverPath = null) {
     this.title = title;
@@ -14,34 +14,18 @@ function Book(title, author, pages, isRead, coverPath = null) {
 function createSampleBooks() {
     const books = [];
     books[0] = new Book('The Hobbit', 'J. R. R. Tolkien', 352, true, 'Hobbit.jpeg');
-    books[1] = new Book('Dune', 'Frank Herbert', 412, false, 'Dune.jpg');
+    books[1] = new Book('Dune', 'Frank Herbert', 412, false, 'Dune.jpeg');
     books[2] = new Book('Harry Potter and the Sorcerer\'s Stone', 'J. K. Rowling', 223, true, 'Harry_Potter.jpg');
     return books;
 }
 
-// function displayBook(book, index) {
-//     const main = document.querySelector('main');
-//     const card = document.createElement(`article`);
-//     const onHover = document.createElement('div');
-//     card.setAttribute('id', index);
-//     const closeButton = document.createElement(`button`);
-//     closeButton.setAttribute('class', 'close');
-//     closeButton.textContent = 'Remove';
-//     const closeIcon = document.createElement('span');
-//     closeIcon.setAttribute('class', 'material-symbols-outlined');
-//     closeIcon.textContent = 'close';
-
-
-//     closeButton.appendChild(closeIcon);
-//     onHover.appendChild(closeButton);
-//     card.appendChild(onHover);
-//     main.appendChild(card);
-// }
-
 function displayBook(book, index) {
     const main = document.querySelector('main');
-    main.insertAdjacentHTML('beforeend',
-        `<article id="${index}">
+    main.insertAdjacentHTML('beforeend', book.coverPath ? createBookWithCoverHTML(book, index) : createBookWithoutCoverHTML(book, index));
+}
+
+function createBookWithCoverHTML(book, index) {
+    return `<article id="${index}">
             <div>
                 <button class="close">
                     Remove
@@ -52,7 +36,25 @@ function displayBook(book, index) {
                     <span class="material-symbols-outlined">${book.isRead ? 'visibility' : 'visibility_off'}</span>
                 </button>
                 <p>View and Edit Info</p>
-            </div>ß
+            </div>
             <img src="./images/${book.coverPath}" alt="Book Cover">
-        </article>`);
+        </article>`
+}
+
+function createBookWithoutCoverHTML(book, index) {
+    return `<article class="no-cover">
+            <div>
+                <button class="close">
+                    Remove
+                    <span class="material-symbols-outlined">close</span>
+                </button>
+                <button class="read-status">
+                    ${book.isRead ? 'Read' : 'Unread'}
+                    <span class="material-symbols-outlined">${book.isRead ? 'visibility' : 'visibility_off'}</span>
+                </button>
+            </div>
+            <p>Title: ${book.title}</p>
+            <p>Author: ${book.author}</p>
+            <p>Pages: ${book.pages}</p>
+        </article>`
 }
